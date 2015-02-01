ac1ba6271162f838cc4518a3d5cb06733fd89986
LukeMcNemee <nemecl@seznam.cz>
2015-02-01 11:16:11 +0100

    travis V.

--- tests/script/bootstrap/arduino.sh
+++ tests/script/bootstrap/arduino.sh
@@ -7,34 +7,38 @@ echo "Installing dependencies for building for the Arduino"
 if [ -z "$ARDUINO_DIR" ] || ! test -e $ARDUINO_DIR || [ $OS == "cygwin" ]; then
 
     echo "Installing Arduino..."
-
-    ARDUINO_BASENAME="arduino-1.0.6"
-    if [ $OS == "cygwin" ]; then
-        ARDUINO_FILE="$ARDUINO_BASENAME-windows".zip
-        EXTRACT_COMMAND="unzip -q"
-    elif [ $OS == "mac" ]; then
-        ARDUINO_FILE="$ARDUINO_BASENAME-macosx".zip
-        EXTRACT_COMMAND="unzip -q"
-    else
-        ARDUINO_FILE="$ARDUINO_BASENAME-linux64".tgz
-        EXTRACT_COMMAND="tar xvf"
-        echo "linux"
-    fi
-
-    ARDUINO_URL=http://downloads.arduino.cc/$ARDUINO_FILE
+    $ARDUINO_URL = "http://arduino.cc/download.php?f=/arduino-1.0.6-linux64.tgz"
+    
+    #ARDUINO_BASENAME="arduino-1.0.6"
+    #if [ $OS == "cygwin" ]; then
+    #    ARDUINO_FILE="$ARDUINO_BASENAME-windows".zip
+    #    EXTRACT_COMMAND="unzip -q"
+    #elif [ $OS == "mac" ]; then
+    #    ARDUINO_FILE="$ARDUINO_BASENAME-macosx".zip
+    #    EXTRACT_COMMAND="unzip -q"
+    #else
+    #    ARDUINO_FILE="$ARDUINO_BASENAME-linux64".tgz
+    #    EXTRACT_COMMAND="tar xvf"
+    #    echo "linux"
+    #fi
+    
+    $ARDUINO_FILE = "arduino-1.0.6-linux64.tgz"
+    
+    #ARDUINO_URL=http://downloads.arduino.cc/$ARDUINO_FILE
 
     _pushd $DEPENDENCIES_FOLDER
     if ! test -e $ARDUINO_FILE
     then
         echo "Downloading Arduino IDE..."
-        download $ARDUINO_URL $ARDUINO_FILE
+        wget $ARDUINO_URL 
     fi
 
     if ! test -d $ARDUINO_BASENAME
     then
         echo "Installing Arduino to local folder..."
-        file $ARDUINO_FILE
-        $EXTRACT_COMMAND $ARDUINO_FILE
+        //file $ARDUINO_FILE
+        ls -la
+        tar xvf *.tgz
         echo "Arduino installed"
     fi
 
