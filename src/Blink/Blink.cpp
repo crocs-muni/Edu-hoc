e02a438a9e06d10ddecd4eb5750b780db1d288d5
LukeMcNemee <nemecl@seznam.cz>
2015-07-16 16:09:11 +0200

    Blink added

---
+++ src/Blink/Blink.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <avr/sleep.h>
+
+#include "SerialUtils.h"
+#include "../common.h"
+
+
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(9, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
