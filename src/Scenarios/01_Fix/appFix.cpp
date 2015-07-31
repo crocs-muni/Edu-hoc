9b4138bb3bb1a80e126af5c9cc91b102ad0b7061
LukeMcNemee <nemecl@seznam.cz>
2015-07-31 12:23:48 +0200

    fixed app completed

--- src/Scenarios/01_Fix/appFix.cpp
+++ src/Scenarios/01_Fix/appFix.cpp
@@ -7,12 +7,21 @@
 #include <../../common.h>
 #include <../scenarios.h>
 
-int counter = 0; //
+
+int counter = 0;
 int msgCounter = 0; 
 
 int nodeID = 0;
 int groupID = 0;
 int parentID = 0;
+
+//APP FIX - hardcoded encryption key
+const uint8_t key[16] =    
+   {0x0f, 0xb0, 0xc0, 0x0f,
+    0xa0, 0xa0, 0xa0, 0xa0,
+    0x00, 0x00, 0xa0, 0xa0,
+    0x00, 0x00, 0x00, 0x00};
+
 
 String message =  "";
 char payload[MAX_MESSAGE_LENGTH] = "";
@@ -24,12 +33,15 @@ StackArray <String> stack = StackArray<String>();
 void setup () {
   Serial.begin(57600);
   su.println("\n[Scenario 01], node started", debug);
-  
+
   nodeID = EEPROM.read(NODE_ID_LOCATION);
   groupID = EEPROM.read(GROUP_ID_LOCATION);
-  
+
 
   rf12_initialize(nodeID, FREQUENCY, groupID);
+
+  //APP FIX - enable encryption or you can implement encryption using some AES lib or something else.
+  rf12_encrypt(key);
 
   while(stack.count() != MESSAGES_COUNT){
       message = Serial.readStringUntil('\n');
@@ -39,16 +51,15 @@ void setup () {
 
 void loop () {  
   while(stack.count() > 1){
-      
-    //while there are messages, send new one every second
+
     message = stack.pop();
     byte hdr = 0;
     message.toCharArray(payload, message.length());
-    
+
     ru.setBroadcast(&hdr);
     ru.resetAck(&hdr);
     rf12_sendNow(hdr, payload, message.length());
-    
+
     su.println("message send: ", debug);
     su.println(payload, debug);
     delay(1000);
