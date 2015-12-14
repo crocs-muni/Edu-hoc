7871fb6b9ba05e6d19c8f9bc4f04dc1bdf68783d
Luke McNemee <nemecl@seznam.cz>
2015-12-15 00:23:27 +0100

    study text pdf added

--- src/Test/test.cpp
+++ src/Test/test.cpp
@@ -1,9 +1,18 @@
 
 #include <Arduino.h>
+#include <EEPROM.h>
 #include "RF12.h"
+#include "RadioUtils.h"
+#include "SerialUtils.h"
+
+#include <../common.h>
+
 
 const byte LED = 9;
 byte counter;
+
+RadioUtils ru = RadioUtils();
+
 
 // turn the on-board LED on or off
 static void led (bool on) {
@@ -13,15 +22,19 @@ static void led (bool on) {
 
 void setup () {
   // this is node 1 in net group 100 on the 868 MHz band
-  rf12_initialize(1, RF12_868MHZ, 50);
+  rf12_initialize(25, RF12_868MHZ, 10);
 }
 
 void loop () {
   led(true);
-  
+  byte hdr = 0;
   // actual packet send: broadcast to all, current counter, 1 byte long
-  rf12_sendNow(0, &counter, 1);;
-  rf12_sendWait(1);
+  String message = String("distance:-10");
+  char payload[15] = "";
+  message.toCharArray(payload, message.length()+1);
+  ru.setBroadcast(&hdr);
+  ru.resetAck(&hdr);
+  rf12_sendNow(hdr, payload, message.length());
 
   led(false);
 
