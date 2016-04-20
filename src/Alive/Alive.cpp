66eff0f460a1d3b9ab72916349797839281204b6
Luke McNemee <nemecl@seznam.cz>
2016-04-20 13:31:52 +0200

    study text update

--- src/Alive/Alive.cpp
+++ src/Alive/Alive.cpp
@@ -5,7 +5,7 @@
 #include <../common.h>
 
 int counter = 0; //
-int msgCounter = 0; 
+int msgCounter = 0;
 
 int id = 0;
 int group = 0;
@@ -26,32 +26,32 @@ void setup () {
 
 void loop () {  
   //if incoming message received
-  
+
   if(rf12_recvDone()){
     if(RF12_WANTS_ACK){
       rf12_sendStart(RF12_ACK_REPLY,0,0);
     }
-    
+
     if(rf12_crc == 0){ //packet checksum is correct
-      //propagate to parent 
+      //propagate to parent
       byte header = B00000000;
-      //fill header using radioUtils      
+      //fill header using radioUtils
       ru.resetAck(&header);
-      ru.setID(&header, parent);      
+      ru.setID(&header, parent);
       rf12_sendNow(header, (const void*)rf12_data, rf12_len);
     }
   }
-  
-  delay(10);  
+
+  delay(10);
   counter++;
-  
+
   if(counter%100 == 0){
     msgCounter++;
     //send still alive msg
     byte header;
-    //fill header using radioUtils    
+    //fill header using radioUtils
     ru.resetAck(&header);
-    ru.setID(&header, parent);  
+    ru.setID(&header, parent);
     rf12_sendNow(header, (const void*) &msgCounter, sizeof(msgCounter));
     counter = 0;
   }
